3705966977012a29699f02f854da200995e747d2
andreasbm <andmehlsen@gmail.com>
2018-10-31 17:28:33 +0100

    Bumped dependencies.

--- typings.d.ts
+++ typings.d.ts
@@ -1,14 +1,1 @@
-declare module "*.scss" {
-	const content: string;
-	export default content;
-}
-
-declare module "*.css" {
-	const content: string;
-	export default content;
-}
-
-declare module "*.json" {
-	const json: any;
-	export default json;
-}
+/// <reference path="node_modules/@appnest/web-config/typings.d.ts" />
