f96e0326ae5f058ca4fafc52deb684414e7e60e7
andreasbm <andmehlsen@gmail.com>
2018-11-10 17:49:36 +0100

    Improved typings of values.

--- src/lib/model.ts
+++ src/lib/model.ts
@@ -1,4 +1,4 @@
-export type Value = object | string;
+export type Value = object | string | number;
 export type ValueCallback = () => Value;
 export type Values = {[key: string]: Value | ValueCallback};
 export type ValuesCallback = () => Values;
